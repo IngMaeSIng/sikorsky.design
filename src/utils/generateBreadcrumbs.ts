f4623a58a3635641b69c7532cd99cfc2efa2e690
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-10-29 16:11:31 +0100

    add breadcrumbs for desktop

---
+++ src/utils/generateBreadcrumbs.ts
@@ -0,0 +1,24 @@
+export function generateBreadcrumbs(currentPath: string, pageTitle: string) {
+    const pathSegments = currentPath.split('/').filter(segment => segment !== '');
+    const breadcrumbs = [{ label: 'Paul Sikorsky', href: '/' }];
+
+    let currentHref = '';
+    for (let i = 0; i < pathSegments.length; i++) {
+        const segment = pathSegments[i];
+        currentHref += `/${segment}`;
+
+        if (i === pathSegments.length - 1) {
+            breadcrumbs.push({
+                label: pageTitle,
+                href: currentHref
+            });
+        } else {
+            breadcrumbs.push({
+                label: segment.charAt(0).toUpperCase() + segment.slice(1),
+                href: currentHref
+            });
+        }
+    }
+
+    return breadcrumbs;
+}
