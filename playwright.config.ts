23f48c28f50a4d4ad5ecc12b429b7a0fcb07eb27
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-11-04 20:56:25 +0100

    specify a platform for screenshots on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'npm run start',
     url: 'http://localhost:4321',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
